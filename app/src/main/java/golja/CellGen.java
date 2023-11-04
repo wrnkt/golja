b257982d634eb8d1d0c27c816b0fa2c991667d40
Niko Ware <wrnkt1@gmail.com>
2023-11-04 01:11:54 -0400

    Fix weighted cell supplier

--- app/src/main/java/golja/CellGen.java
+++ app/src/main/java/golja/CellGen.java
@@ -1,10 +1,14 @@
 package golja;
 
+import java.util.Map;
 import java.util.function.Supplier;
-import golja.CellStateGen;
 
 @FunctionalInterface
 interface CellSupplier extends Supplier<Cell> {}
+@FunctionalInterface
+interface WeightedCellSupplier {
+  Cell get(Map <State, Integer> weights);
+}
 
 public final class CellGen {
   public static final CellSupplier DEAD_CELL_SUPPLIER = () -> new Cell(State.DEAD);
@@ -14,7 +18,7 @@ public final class CellGen {
   public static final CellSupplier PSEUDO_EVEN_CELL_SUPPLIER =
   () -> new Cell(CellStateGen.PSEUDO_EVEN_CELL_STATE_SUPPLIER.get());
 
-  public static final CellSupplier WEIGHTED_CELL_SUPPLIER =
-  () -> new Cell(CellStateGen.PSEUDO_EVEN_CELL_STATE_SUPPLIER.get());
+  public static final WeightedCellSupplier WEIGHTED_CELL_SUPPLIER =
+  (Map <State, Integer> weights) -> new Cell(CellStateGen.WEIGHTED_CELL_STATE_SUPPLIER.get(weights));
 
 }
