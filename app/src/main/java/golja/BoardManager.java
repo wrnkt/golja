dd30e67002655508d65780cdd974ddaf1ac4393c
Niko Ware <wrnkt1@gmail.com>
2023-11-05 04:28:30 -0500

    Construct standard game board

--- app/src/main/java/golja/BoardManager.java
+++ app/src/main/java/golja/BoardManager.java
@@ -1,6 +1,7 @@
 package golja;
 
 import java.util.function.*;
+import java.util.List;
 
 
 public class BoardManager
@@ -31,22 +32,23 @@ public class BoardManager
         board.update(rule);
     }
 
-    public static Board randomBoard(int columns, int rows, BooleanSupplier lifeChance)
-    {
-        Board board = new Board(columns, rows);
+    public static Board constructStandardBoard(int cols, int rows) {
+      return constructBoardWithStates(cols, rows, List.of(State.DEAD, State.ALIVE));
+    }
 
-        for(int r = 0; r < rows; r++)
-        {
-            for(int c = 0; c < columns; c++)
-            {
-                State state = lifeChance.getAsBoolean() ?
-                    State.ALIVE :
-                    State.DEAD;
-
-                board.at(c, r).setState(state);
-            }
+    public static Board constructBoardWithStates(
+      int cols,
+      int rows,
+      List<State> states
+    ) {
+      Board board = new Board(cols, rows);
+      SpecifiedCellStateSupplier s = CellStateGen.SPECIFIED_EVEN_PROB_SUPPLIER;
+      for(int r = 0; r < rows; ++r) {
+        for(int c = 0; c < cols; ++c) {
+          board.at(c, r).setState(s.get(states));
         }
-        return board;
+      }
+      return board;
     }
 
 }
