ec9566e14c47a49bfbd3f572fdb570328a6fa0f4
Niko Ware <wrnkt1@gmail.com>
2023-11-17 02:17:08 -0500

    Re add BoardCell

---
+++ app/src/main/java/golja/ui/BoardCell.java
@@ -0,0 +1,25 @@
+package golja.ui;
+
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class BoardCell extends Pane
+{
+  public BoardCell()
+  {
+    final Rectangle rect = new Rectangle();
+    //rect.widthProperty().bind(this.widthProperty());
+    //rect.heightProperty().bind(this.heightProperty());
+
+    //rect.setFill(Color.AQUA);
+
+    this.getChildren().add(rect);
+
+    this.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.DASHED, null, null)));
+  }
+
+}
