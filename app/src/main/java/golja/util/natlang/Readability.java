5b2dc145a4647382438facf52dba5cd2e4998076
Niko Ware <wrnkt1@gmail.com>
2023-11-27 22:22:14 -0500

    Refactor conjunction list printing funcs

--- app/src/main/java/golja/util/natlang/Readability.java
+++ app/src/main/java/golja/util/natlang/Readability.java
@@ -4,27 +4,6 @@ import java.util.List;
 
 public class Readability {
 
-    static public String toReadableList(List<Integer> numbers)
-    {
-        int len = numbers.size();
-        if ( len == 0 ) return "";
-        if ( len == 1 ) return numbers.get(0).toString();
-        if ( len == 2 ) return numbers.get(0).toString() + " and " + numbers.get(1).toString();
-
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < len; ++i) {
-            sb.append(numbers.get(i));
-            if ( i == len - 2 ) {
-                sb.append(", and ");
-            } else if ( i == len - 1 ) {
-                // last value
-            } else {
-                sb.append(", ");
-            }
-        }
-        return sb.toString();
-    }
-
     static public String toListWithConjunction(List<Integer> numbers, String conjunction)
     {
         int len = numbers.size();
@@ -44,5 +23,15 @@ public class Readability {
             }
         }
         return sb.toString();
+    }
+
+    static public String toListAnd(List<Integer> numbers)
+    {
+        return toListWithConjunction(numbers, "and");
+    }
+
+    static public String toListOr(List<Integer> numbers)
+    {
+        return toListWithConjunction(numbers, "or");
     }
 }
