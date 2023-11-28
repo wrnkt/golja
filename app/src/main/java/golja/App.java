1e46bc9c47c0332930e257a2a12cf0ea555b34d2
Niko Ware <wrnkt1@gmail.com>
2023-11-27 20:56:44 -0500

    Run console app in main class

--- app/src/main/java/golja/App.java
+++ app/src/main/java/golja/App.java
@@ -1,23 +1,31 @@
 package golja;
 
+import java.io.IOException;
+
 import golja.console.BoardPrinter;
 import golja.model.board.BoardManager;
+import golja.console.ConsoleApp;
 
 public class App {
     public static void main(String[] args) {
-      //Golja.main(args);
-      try
-      {
-          BoardPrinter.animateBoard(
-            BoardManager.constructStandardBoard(
-              BoardPrinter.DEFAULT_COLS,
-              BoardPrinter.DEFAULT_ROWS
-            )
-          );
-      }
-      catch (InterruptedException e)
-      {
-          System.out.println("Failed to animate");
-      }
+        try {
+            ConsoleApp.main(args);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+          //Golja.main(args);
+          // try
+          // {
+          //     BoardPrinter.animateBoard(
+          //       BoardManager.constructStandardBoard(
+          //         BoardPrinter.DEFAULT_COLS,
+          //         BoardPrinter.DEFAULT_ROWS
+          //       )
+          //     );
+          // }
+          // catch (InterruptedException e)
+          // {
+          //     System.out.println("Failed to animate");
+          // }
     }
 }
