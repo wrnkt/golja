2816115039bf273b957c3d10d889e22b192d74f9
Niko Ware <wrnkt1@gmail.com>
2023-11-27 22:16:18 -0500

    Redefine default rule

--- app/src/main/java/golja/rule/Rules.java
+++ app/src/main/java/golja/rule/Rules.java
@@ -1,8 +1,50 @@
 package golja.rule;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import golja.model.cell.State;
 
+
 public final class Rules {
+
+    static Rule DEFAULT_RULE_W = new Rule() {
+
+        @Override
+        public String title() {
+            return "Default Rule";
+        }
+
+        @Override
+        public Map<State, List<State>> stateMap() {
+            Map<State, List<State>> sm = new HashMap<>();
+            sm.put(
+                State.ALIVE,
+                List.of(
+                    State.DEAD,
+                    State.DEAD, State.ALIVE, State.ALIVE, State.DEAD, 
+                    State.DEAD, State.DEAD, State.DEAD, State.DEAD
+                )
+            );
+            sm.put(
+                State.DEAD,
+                List.of(
+                    State.DEAD,
+                    State.DEAD, State.DEAD, State.ALIVE, State.DEAD,
+                    State.DEAD, State.DEAD, State.DEAD, State.DEAD
+                )
+            );
+            return sm;
+        }
+    };
+
+    public static final List<Rule> AVAILABLE_RULES = new ArrayList<>();
+
+    static {
+        AVAILABLE_RULES.add(DEFAULT_RULE_W);
+    };
 
     public static AppliableRule DEFAULT_RULE = (c, r, b) -> {
         int aliveNeighbors = b.neighborsWithState(c, r, State.ALIVE);
@@ -21,6 +63,5 @@ public final class Rules {
                 return State.DEAD;
         }
     };
-
   
 }
